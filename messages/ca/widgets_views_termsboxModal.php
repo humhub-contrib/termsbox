7c63eee4ddaa3d478ecd68a7f261a2d99f9c9582
HumHub Translations <info@humhub.com>
2021-02-15 18:24:47 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/ca/widgets_views_termsboxModal.php
+++ messages/ca/widgets_views_termsboxModal.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'Accept' => '',
+  'Accept' => 'Accepta',
   'Decline' => 'No hi assistiré',
 );
--- messages/pt/models_forms_EditForm.php
+++ messages/pt/models_forms_EditForm.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Active' => 'Activado',
-  'Content' => '',
+  'Content' => 'Conteúdo',
   'Hide users which not accepted the terms (Note: May require search index rebuild)' => '',
   'Mark as unseen for all users' => 'Marcar como invisível para todos os utilizadores',
   'Please Read and Agree to our Terms & Conditions' => '',
