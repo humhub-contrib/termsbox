7035d4d7ac147362e0cf535196eacd4be41442df
HumHub Translations <info@humhub.com>
2021-07-17 21:50:23 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/models_forms_EditForm.php
+++ messages/it/models_forms_EditForm.php
@@ -2,11 +2,11 @@
 return array (
   'Active' => 'Attivo',
   'Content' => 'Contenuto',
-  'Hide users which not accepted the terms (Note: May require search index rebuild)' => '',
+  'Hide users which not accepted the terms (Note: May require search index rebuild)' => 'Nascondi gli utenti che non hanno accettato i termini (Nota: potrebbe richiedere la ricostruzione dell\'indice di ricerca)',
   'Mark as unseen for all users' => 'Marca come non visto per tutti gli utenti',
-  'Please Read and Agree to our Terms & Conditions' => '',
-  'Show terms as modal' => '',
-  'Statement' => '',
-  'Terms & Conditions' => '',
+  'Please Read and Agree to our Terms & Conditions' => 'Si prega di leggere e accettare i nostri Termini e condizioni',
+  'Show terms as modal' => 'Mostra termini come modali',
+  'Statement' => 'dichiarazione',
+  'Terms & Conditions' => 'Termini &amp; Condizioni',
   'Title' => 'Titolo',
 );
--- messages/it/views_admin_index.php
+++ messages/it/views_admin_index.php
@@ -2,5 +2,5 @@
 return array (
   'Back to modules' => 'Indietro ai moduli',
   'Save' => 'Salva',
-  'Terms Box Configuration' => '',
+  'Terms Box Configuration' => 'Configurazione della casella dei termini',
 );
