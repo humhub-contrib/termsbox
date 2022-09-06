c326d613ed0589b61d7473f9d6d67f2c67c52ba8
HumHub Translations <hello@humhub.com>
2022-09-06 16:33:06 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/models_forms_EditForm.php
+++ messages/pt/models_forms_EditForm.php
@@ -2,11 +2,11 @@
 return array (
   'Active' => 'Activado',
   'Content' => 'Conteúdo',
-  'Hide users which not accepted the terms (Note: May require search index rebuild)' => '',
+  'Hide users which not accepted the terms (Note: May require search index rebuild)' => 'Ocultar utilizadores que não aceitarem os termos. (Nota: Pode ser necessário reconstruir o índice de pesquisa)',
   'Mark as unseen for all users' => 'Marcar como invisível para todos os utilizadores',
-  'Please Read and Agree to our Terms & Conditions' => '',
-  'Show terms as modal' => '',
-  'Statement' => '',
-  'Terms & Conditions' => '',
+  'Please Read and Agree to our Terms & Conditions' => 'Por favor leia e concorde com os nossos termos e condições',
+  'Show terms as modal' => 'Exibir termos como modal',
+  'Statement' => 'Declaração',
+  'Terms & Conditions' => 'Termos &amp; Condições',
   'Title' => 'Título',
 );
