cf7fce5d4104cad8f497d31c8b466fe48eb1fb7a
HumHub Translations <hello@humhub.com>
2023-05-03 17:00:14 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/models_forms_EditForm.php
+++ messages/ja/models_forms_EditForm.php
@@ -3,7 +3,7 @@ return array (
   'Active' => 'アクティブ',
   'Content' => 'コンテンツ',
   'Hide users which not accepted the terms (Note: May require search index rebuild)' => '条件に同意しなかったユーザーを非表示にする (注：検索インデックスの再構築が必要になる場合があります)',
-  'Mark as unseen for all users' => '全てのユーザーに見えない状態としてマーク',
+  'Mark as unseen for all users' => 'すべてのユーザーに見えない状態としてマーク',
   'Please Read and Agree to our Terms & Conditions' => '利用規約を読んで同意してください',
   'Show terms as modal' => '用語をモーダルで表示',
   'Statement' => 'ステートメント',
